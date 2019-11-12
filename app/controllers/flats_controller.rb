47bb3c0bebdd1b749208737edb980771a6bec707
Mohammed Nouri <simoiroun25@gmail.com>
2019-11-12 18:53:02 +0100

    popup stylsheet

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,7 +8,7 @@ class FlatsController < ApplicationController
     @markers = @flats.map do |flat|
       { lat: flat.latitude,
         lng: flat.longitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { flat: flat }) }
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }) }
     end
   end
 
